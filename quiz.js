05f06f3df3ab65ac6bac0928cd58c68f08209116
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-03 23:17:08 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer(){
     let correctAnswer = "4";
     userAnswer=document.querySelector('input[name="quiz"]:checked')?.value;
 
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         document.querySelector('#feedback').textContent = "Correct! Well done.";
     }else{
         document.querySelector('#feedback').textContent = "That's incorrect. Try again! ";
