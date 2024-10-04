f557bb6cd0fb67fa65a418a5ee8eeb5c16a56a69
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-10-04 18:34:14 +0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -7,7 +7,6 @@ function checkAnswer(){
         document.querySelector('#feedback').textContent = "Correct! Well done.";
     }else{
         document.querySelector('#feedback').textContent = "That's incorrect. Try again! ";
-
     }
 }
 document.getElementById("submit-answer").addEventListener("click", checkAnswer);
